a120a2b89253fc82218a609ab70eb05db31243f4
Russia Madden <dataengineer007@gmail.com>
2017-01-23 00:40:24 +0000

    first templates

---
+++ collection/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    # this is your new view
+    number = 6
+    thing = "Thing name"
+    return render(request, 'index.html', {'number': number, 'thing': thing,
+    })
