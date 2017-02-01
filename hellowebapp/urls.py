8cf5998123a16ce2dac66c85e9adf680f5661eab
Russia Madden <dataengineer007@gmail.com>
2017-02-01 05:28:10 +0000

    fixed post on submit fail

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -19,10 +19,12 @@ from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    url(r'^$', views.index, name='home'),
-    url(r'^admin/', admin.site.urls),
     url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'), 
+    url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing, name='edit_thing'),
+    
+    
     url(r'^admin/', admin.site.urls),
 ]
