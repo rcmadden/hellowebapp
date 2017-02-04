3ec651ed48afe237a7af3c1ea4ce2589dffc8713
Russia Madden <dataengineer007@gmail.com>
2017-02-04 04:52:24 +0000

    loging page settings file

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'collection',
-    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
